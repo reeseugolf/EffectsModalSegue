0797eeadcef8caa5e2383c4a12eb8dee41cf9d8c
reeseugolf <15817004195@163.com>
2015-07-17 10:11:28 +0800

    Initial Commit

---
+++ EffectsModalSegue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EffectsModalSegue
+//
+//  Created by UGolf_Reese on 15/7/17.
+//  Copyright (c) 2015年 reese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
